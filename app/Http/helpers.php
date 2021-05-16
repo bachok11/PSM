cd7a6f0ba48bb0217a8d14d9122ba7aa2452cc54
bachok11 <m.amin.a.hamid@gmail.com>
2021-05-16 10:21:52 +0800

    Update UI dashboard

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,3 +1,59 @@
 <?php
-    getMosqueName();
+
+// Get active Admin list in data list
+if (!function_exists('_getActiveSuperAdmin')) {
+	function _getActiveSuperAdmin($id)
+	{	
+		$data  = DB::table('users')->where('id','=',$id)->first();
+		if(!empty($data))
+		{
+			$userrole = $data->role;
+			if($userrole == 'Super Admin')
+			{
+				return "yes";
+			}
+			else
+			{	
+				return "no";
+			}
+		}
+	}
+}
+
+// Get quantity of Mosque Committee
+if (!function_exists('getMosqueCommitteeQuantity')) {
+    function getMosqueCommitteeQuantity()
+    {
+        $query = DB::table('tbl_mosque_committee')->count();
+        if(!empty($query))
+		{
+			return $query;
+		}
+    }
+}
+
+// Get quantity of Quran Teachers
+if (!function_exists('getQuranTeachersQuantity')) {
+    function getQuranTeachersQuantity()
+    {
+        $query = DB::table('tbl_quran_teachers')->count();
+        if(!empty($query))
+		{
+			return $query;
+		}
+    }
+}
+
+// Get quantity of Hafiz
+if (!function_exists('getHafizQuantity')) {
+    function getHafizQuantityQuantity()
+    {
+        $query = DB::table('tbl_hafiz')->count();
+        if(!empty($query))
+		{
+			return $query;
+		}
+    }
+}
+
 ?>
